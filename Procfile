78e429e803a28e18e8049fe8ff9d124b5051ab12
Admin <admin@Henrik-MBPro.local>
2012-01-12 18:35:39 +0100

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: lein run -m bf3.core
---
+++ test/bf3/test/core.clj
@@ -0,0 +1,6 @@
+(ns bf3.test.core
+  (:use [bf3.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
