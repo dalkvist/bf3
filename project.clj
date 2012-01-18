02e58b7100418d60abef36401172eca76da01005
Mattias dalkvist <mattias@linux-MacBookPro.(none)>
2012-01-18 20:32:06 +0100

    bf3stats

--- project.clj
+++ project.clj
@@ -1,3 +1,12 @@
 (defproject bf3 "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [cheshire "2.0.6"]
+                 [clj-http "0.2.7"]
+                 [noir "1.2.2"]
+                 [congomongo "0.1.7"]
+                 [clj-stacktrace "0.2.4"]]
+    :jvm-opts ["-Xms350m" "-Xmx350m" "-server" "-XX:+UseConcMarkSweepGC"
+             "-XX:+CMSIncrementalMode" "-XX:+UseCompressedOops"
+             "-XX:+DoEscapeAnalysis" "-XX:+UseBiasedLocking"
+             "-XX:PermSize=64M" "-XX:MaxPermSize=85M"])
