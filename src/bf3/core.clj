fef1468848634875736aafe870b39fac6019c8c3
Mattias Dalkvist <mattias@nividev.com>
2012-01-20 18:23:49 +0100

    web, basic info

--- src/bf3/core.clj
+++ src/bf3/core.clj
@@ -13,12 +13,30 @@
 (defpartial layout [& content]
             (html5
               [:head
-               [:title "Battlefield 3"]
+               [:title "Battlefield 3 loadout randomizer"]
                ;; (include-css "/css/reset.css")
                ]
               [:body
                [:div#wrapper
-                content]]))
+                [:div#alphaInfo
+                 [:b "Simple battlefield 3 loadout randomizer"]
+                 [:p "use bf3.herokuapp.com for random loadout with all unlocks, inc all attachments"]
+                 [:p "or use bf3.herokuapp.com/YOURSOLDIER "
+                  "for a loadout with only the stuff you have unlocked"]
+                 [:p "you need to manually call an update on bf3stats.com"]]
+                [:div#content content]]
+               [:div#todo [:b "todo"]
+                (unordered-list ["design ;)"
+                                 "fix attachments on PDWs (some have attachments on diferent slots then the main weapons)"
+                                 "vehicles" "configuration:"
+                                 (unordered-list ["classes"
+                                                  "prefere weapons/classes without 1/5 service star(s)"
+                                                  "prefere weapons with unlocked attachments"
+                                                  "add update call to bf3stats"
+                                                  "vehicle loadouts"
+                                                  "squad loadouts"])])]
+               [:div#fotter 
+                 [:p "data from bf3stats.com"]]]))
 
 
 (defroutes public-routes
