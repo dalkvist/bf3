738785874ef4f448092742f3e77b3dc312e08089
Mattias Dalkvist <mattias@nividev.com>
2012-03-05 14:40:06 +0100

    gc, saving more bl info

--- src/bf3/bl.clj
+++ src/bf3/bl.clj
@@ -10,6 +10,33 @@
         [bf3.db])
   (:import java.net.URL))
 
+
+(def ^{:dynamic true} *cache-time* (* 2 60 1000))
+
+(def maps {
+           "MP_001" "Grand Bazaar"
+           "MP_003" "Teheran Highway"
+           "MP_007" "Caspian Border"
+           "MP_011" "Seine Crossing"
+           "MP_012" "Operation Firestorm"
+           "MP_013" "Damavand Peak"
+           "MP_017" "Noshahar Canals"
+           "MP_018" "Kharg Island"
+           "MP_Subway" "Operation Metro"
+           "XP1_001" "Strike At Karkand"
+           "XP1_002" "Gulf of Oman"
+           "XP1_003" "Sharqi Peninsula"
+           "XP1_004" "Wake Island"
+           })
+
+(def mapModes {1  "Conquest"
+               2  "Rush"
+               4  "Squad Rush"
+               8  "Squad DM"
+               32 "Team DM"
+               64 "Conquest Large"
+           })
+
 (defn- get-current-iso-8601-date
   "Returns current ISO 8601 compliant date."
   []
@@ -21,27 +48,28 @@
 
 (def players-on-server-url "http://battlelog.battlefield.com/bf3/servers/getPlayersOnServer/")
 
-(defn- get-players-on-server [server-id]
-  (-> (client/get (str players-on-server-url server-id "/" ))
-      :body
-      (parse-string true)))
+(defn- get-server-info [id]
+  (-> (client/get (str "http://battlelog.battlefield.com/bf3/servers/show/" id "/a/")
+                  {:headers {"X-AjaxNavigation" "1"}})
+      :body (parse-string true) :context))
+
+(def server-info (mem/memo-ttl get-server-info *cache-time*))
 
 (def server-ids (hash-map
            :eu          "bc442e68-e072-4151-86da-cdba81c51cf5"
            :na-chicago  "14fb6dc4-1f57-4920-938b-73b0921b9303"
            :na-new-york "2da0845c-463d-4452-93ab-51294898474d"))
 
-(def ^{:dynamic true} *cache-time* (* 2 60 1000))
-
 (defn- get-live-users
   "get user from the battlelog server"
   ([] (get-live-users (->> server-ids vals first)))
   ([id]
      (hash-map :time (get-current-iso-8601-date)
-               :users (->> (get-players-on-server id)
-                           :players
-                           (map #(-> % :persona :personaId)))
-               :server id)))
+               :users (->>  id server-info :players
+                            (map #(-> % :persona :personaId)))
+               :server id
+               :info (-> id (server-info) :server
+                         (select-keys  [:hasPassword :gameId :map :mapMode])))))
 
 (def get-users (mem/memo-ttl get-live-users *cache-time*))
 
