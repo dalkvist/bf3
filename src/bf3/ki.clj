4f44460515cf7f42211269a707d4cb7a74bacaaa
Mattias Dalkvist <mattias@nividev.com>
2012-03-06 13:41:19 +0100

    rename k1 -> ki

--- src/bf3/ki.clj
+++ src/bf3/ki.clj
@@ -1,4 +1,4 @@
-(ns bf3.k1
+(ns bf3.ki
   (:require [clj-http.client :as client]
             [clojure.string :as s]
             [clojure.core.cache :as cache]
@@ -16,9 +16,9 @@
 
 (def api-url "https://spreadsheets.google.com/feeds/download/spreadsheets/Export?key=0AolGselmDdqedDhnMkZGNXdZQWFON3JoTjd0WGs3c0E&exportFormat=csv")
 
-(def k1-url "https://docs.google.com/spreadsheet/ccc?key=0AolGselmDdqedDhnMkZGNXdZQWFON3JoTjd0WGs3c0E")
+(def ki-url "https://docs.google.com/spreadsheet/ccc?key=0AolGselmDdqedDhnMkZGNXdZQWFON3JoTjd0WGs3c0E")
 
-(def k1-url-2 "https://docs.google.com/spreadsheet/lv?key=0AolGselmDdqedDhnMkZGNXdZQWFON3JoTjd0WGs3c0E")
+(def ki-url-2 "https://docs.google.com/spreadsheet/lv?key=0AolGselmDdqedDhnMkZGNXdZQWFON3JoTjd0WGs3c0E")
 
 (def ^{:dynamic true} *cache-time* (* 30 60 1000))
 
@@ -27,17 +27,17 @@
 (defn- get-player-info [_ f-name o-name rank branch _ jet halo armor _]
   (struct player f-name o-name rank branch (not (empty? jet)) (not (empty? halo)) (not (empty? armor))))
 
-(defn- get-k1-players []
-  (->> (.getByXPath (.getPage @client k1-url-2)
+(defn- get-ki-players []
+  (->> (.getByXPath (.getPage @client ki-url-2)
         "//table[@id='tblMain']//tr") (drop 1)
        (map (fn [row] (->>  row .getCells (map #(.asText %)) (apply get-player-info))))))
 
-(def k1-players (mem/memo-ttl get-k1-players *cache-time*))
+(def ki-players (mem/memo-ttl get-ki-players *cache-time*))
 
-(defn- k1-info [player-name]
-  "get the k1 info from the google document"
+(defn- ki-info [player-name]
+  "get the ki info from the google document"
   (first (filter #(or  (= (s/lower-case player-name) (s/lower-case (:forum-name %)))
                        (= (s/lower-case player-name) (s/lower-case (:origin-name %))))
-                 (k1-players))))
+                 (ki-players))))
 
-(def get-k1-info (mem/memo-ttl k1-info *cache-time*))
+(def get-ki-info (mem/memo-ttl ki-info *cache-time*))
